739036249d7fd03042b4a6c49e604559ddfdffe3
Dhruv Chadha <dhruvchadha2212@hotmail.com>
2017-03-16 21:21:33 +0530

    Calendar module
    Will combine with backend on return to campus.
    Currently, checks timeslots available on a particular date and let select available timeslot.

---
+++ Calendar module/find.php
@@ -0,0 +1,17 @@
+<?php
+	$username = 'root';
+	$password = '';
+	$db = 'bsbe_db';
+	//print_r ($_GET);
+	$conn = new mysqli('localhost', $username, $password, $db);
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	}
+	list($month,$day,$year)=explode("/",$_GET['q']);
+	$date = $year."-".$month."-".$day;
+	$sql = "select Id from timetable where Date = '" . $date . "';";
+	//echo "<br>".$sql;
+	$result = $conn->query($sql);
+	$row = mysqli_fetch_assoc($result); 
+	echo json_encode($row);
+?>
