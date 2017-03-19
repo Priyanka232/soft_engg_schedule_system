b42f820965c8e7273d249d8539a53ed5ced17607
Dhruv Chadha <dhruvchadha2212@hotmail.com>
2017-03-19 23:05:11 +0530

    main page css

--- 2nd iteration without GUI/first.php
+++ 2nd iteration without GUI/first.php
@@ -1,52 +1,54 @@
 <?php
-include 'sql.php';
-
-session_start();
-
+	include 'sql.php';
+	session_start();
 ?>
 
 <html>
+
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+	<link href = "first_css.css" rel = "stylesheet" type = "text/css">
+</head>
+
 <body>
-<br>
-<button onclick = "display_new_user_form()" type = "button" id = "new_user_display">New User</button>
-<button onclick = "display_existing_user_form()" type = "button" id = "existing_user_display">Existing User</button>
-<br>
+	<header>
+		<div class="wrapper">
+			<div class="logo"><img src="logo.png" alt="logo"></div> 
+			</br></br></br>
+			</br>  
+			<div>
+				<p align="right" class="style1"><font color="#FFFFFF" size="5px">Biosciences & Biomedical Engineering</font></p>    
+			</div>
+		</div>
+	</header>
+	<!--header finish (iiti logo)-->
+	
+	<div id = "buttons">
+		<button id = "button1" onclick = "display_new_user_form()" type = "button" id = "new_user_display">New User</button>
+		<button id = "button2" onclick = "display_existing_user_form()" type = "button" id = "existing_user_display">Existing User</button>
+		<a href = "checkthebooked.php"><p align = "center">Previous Bookings</p></a>
+	</div>
+	
+	<h1 id = "show_status">
+		<?php
+			echo isset($_GET['status']) ? $_GET['status'] : '';		//shows status whether existing user is found or not
+		?>
+	</h1>
 
-<h1 id = "show_status"><?php
-	echo isset($_GET['status']) ? $_GET['status'] : '';
-?></h1>
+	<form id = "new_user_form" action="check.php" method="post">
+		<input placeholder = "E-Mail" type="text" name="email" maxlength = "50" required><br>
+		<input placeholder = "Name" type="text" name="name" maxlength = "30" required><br>
+		<input placeholder = "Phone Number" type="text" name="phone" required><br>
+		<input placeholder = "Address" type="text" name="address" maxlength = "100" required><br>
+		<input type="submit">
+	</form>
 
-<form style = "display: none;" id = "new_user_form" action="check.php" method="post">
-E-mail: <input type="text" name="email" maxlength = "50" required><br>
-Name: <input type="text" name="name" maxlength = "30" required><br>
-Phone Number: <input type="text" name="phone" required><br>
-Address: <input type="text" name="address" maxlength = "100" required><br>
-<input type="submit">
-</form>
+	<form id = "existing_user_form" action="check.php" method="post">
+		<input placeholder = "E-Mail" type="text" name="email" maxlength = "50" required><br>
+		<input type="submit">
+	</form>
 
-<form style = "display: none;" id = "existing_user_form" action="check.php" method="post">
-E-mail: <input type="text" name="email" maxlength = "50" required><br>
-<input type="submit">
-</form>
 
-<p>Check the booking status :<a href = "checkthebooked.php">click here</a></p>
-
-<script>
-	var new_user_display = document.getElementById("new_user_display");
-	var existing_user_display = document.getElementById("existing_user_display");
-	var new_user_form = document.getElementById("new_user_form");
-	var existing_user_form = document.getElementById("existing_user_form");
-	var show_status = document.getElementById("show_status");
-	function display_new_user_form() {
-		new_user_form.style.display = "initial";
-		existing_user_form.style.display = "none";
-		show_status.style.display = "none";
-	}
-	function display_existing_user_form() {
-		new_user_form.style.display = "none";
-		existing_user_form.style.display = "initial";
-		show_status.style.display = "none";
-	}
-</script>
+	<script src = "first_js.js"></script>
 </body>
 </html>
